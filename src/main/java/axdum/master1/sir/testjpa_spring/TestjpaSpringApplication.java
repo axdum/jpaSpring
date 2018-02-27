94db3bc0b2a8a8bb0cf062a9a58aec5888aac4a9
axdum <axel.dumont3@gmail.com>
2018-02-28 00:55:42 +0100

    Initial commit

---
+++ src/main/java/axdum/master1/sir/testjpa_spring/TestjpaSpringApplication.java
@@ -0,0 +1,12 @@
+package axdum.master1.sir.testjpa_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjpaSpringApplication.class, args);
+	}
+}
